9bb378292b763daa056fbea139eb314749aa6536
Steven Senkowski <scsenkowski@gmail.com>
2017-11-09 23:58:11 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,8 @@
-def my_each # put argument(s) here
-  # code here
+counter = 0
+
+def my_each(words) # put argument(s) here
+  while condition
+    my_each(words) do |word|
+      puts word
+  end
 end
