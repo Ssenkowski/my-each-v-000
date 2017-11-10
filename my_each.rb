647b825525aefe9f41ee95975371e5daba5e7811
Steven Senkowski <scsenkowski@gmail.com>
2017-11-10 00:01:26 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -4,7 +4,7 @@ def my_each(words) # put argument(s) here
   words = ['hi', 'hello', 'bye', 'goodbye']
     while counter < 5
       my_each(words) do |word|
-      puts word
+        word
       counter += 1
       end
     end
